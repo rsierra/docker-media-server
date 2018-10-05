17fbe048e281992bd53defa4852a683dc69bb380
Rubén Sierra <maguilag@gmail.com>
2018-10-05 15:47:45 +0200

    Use traefik network as local for compose

--- media-server.sh
+++ media-server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export $(cat .env | grep -v ^# | xargs)
+envsubst < templates/config/ddclient/ddclient.conf
 
 CONFIGURATIONS="-f docker-compose.system.yml -f docker-compose.media.yml -f docker-compose.downloads.yml"
 
